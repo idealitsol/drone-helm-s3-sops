98db18d384ada7f76faca173c30178d9a3208af0
E.K.B <bekintest@gmail.com>
2019-11-11 23:36:06 +0000

    removing what could potentially be the issue

--- run.sh
+++ run.sh
@@ -2,38 +2,38 @@
 
 set -e
 
-if [[ ! -z ${KUBERNETES_TOKEN} ]]; then
+if [[ ! -z $KUBERNETES_TOKEN ]]; then
   KUBERNETES_TOKEN=$KUBERNETES_TOKEN
 fi
 
-if [[ ! -z ${KUBERNETES_SERVER} ]]; then
-  KUBERNETES_SERVER=${KUBERNETES_SERVER}
+if [[ ! -z $KUBERNETES_SERVER ]]; then
+  KUBERNETES_SERVER=$KUBERNETES_SERVER
 fi
 
-if [[ ! -z ${KUBERNETES_CERT} ]]; then
-  KUBERNETES_CERT=${KUBERNETES_CERT}
+if [[ ! -z $KUBERNETES_CERT ]]; then
+  KUBERNETES_CERT=$KUBERNETES_CERT
 fi
 
-kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
-if [[ -z ${KUBERNETES_CERT} ]]; then
-  echo ${KUBERNETES_CERT} | base64 -d >ca.crt
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
+kubectl config set-credentials default --token=$KUBERNETES_TOKEN
+if [[ -z $KUBERNETES_CERT ]]; then
+  echo $KUBERNETES_CERT | base64 -d >ca.crt
+  kubectl config set-cluster default --server=$KUBERNETES_SERVER --certificate-authority=ca.crt
 else
   echo "WARNING: Using insecure connection to cluster"
-  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+  kubectl config set-cluster default --server=$KUBERNETES_SERVER --insecure-skip-tls-verify=true
 fi
 
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
 
 # Run kubectl command
-if [[ ! -z ${PLUGIN_KUBECTL} ]]; then
-  echo ${KUBERNETES_SERVER}
+if [[ ! -z $PLUGIN_KUBECTL ]]; then
+  echo $KUBERNETES_SERVER
   kubectl config view
-  kubectl ${PLUGIN_KUBECTL}
+  kubectl $PLUGIN_KUBECTL
 fi
 
 # Run helm command
-if [[ ! -z ${PLUGIN_HELM} ]]; then
+if [[ ! -z $PLUGIN_HELM ]]; then
   helm version
 fi
